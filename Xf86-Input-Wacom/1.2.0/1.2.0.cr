c182227edef8cf78cebfc59a08d1ff470b86caf6
Fulgurance <zohran.londais@gmail.com>
2023-11-20 21:51:15 +0000

    Added new updates

---
+++ Xf86-Input-Wacom/1.2.0/1.2.0.cr
@@ -0,0 +1,26 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--sysconfdir=/etc",
+                            "--localstatedir=/var",
+                            "--disable-static",
+                            "--with-systemd-unit-dir=no"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
